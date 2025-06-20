3f3fe31f05f5524100856b1867632a41806b1109
unknown <hansel.davis62@gmail.com>
2025-06-19 17:24:25 -0700

    Add random quote generator

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const button = document.getElementById("clickButton");
 const response = document.getElementById("responseText");
 
+const quotes = [
+    "The only limit is your imagination.",
+  "Push yourself, because no one else is going to do it for you.",
+  "Dream it. Wish it. Do it.",
+  "Success doesn’t just find you. You have to go out and get it.",
+  "Don’t stop when you’re tired. Stop when you’re done."
+]
+
 button.addEventListener("click", function () {
-  response.textContent = "You clicked the button!";
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    response.textContent = quotes[randomIndex];
 });
