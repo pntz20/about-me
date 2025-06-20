4d106645b455c12a5532be8d035c8b3a8fa09f72
unknown <hansel.davis62@gmail.com>
2025-06-20 09:55:00 -0700

    Add Age Calculator Script and Update Index

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ button.addEventListener("click", function () {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     response.textContent = quotes[randomIndex];
 });
+
+const checkButton = document.getElementById("checkButton");
+const ageInput = document.getElementById("ageInput");
+const result = document.getElementById("ageResult");
+
+checkButton.addEventListener("click", function () {
+  const age = parseInt(ageInput.value);
+
+  if (isNaN(age)) {
+    result.textContent = "Please enter a valid number.";
+  } else if (age >= 1 && age < 13) {
+    result.textContent = "You're a kid!";
+  } else if (age >= 14 && age <= 17) {
+    result.textContent = "You're a teenager!";
+  } else if (age >= 22 && age < 60) {
+    result.textContent = "You're an adult!";
+  } else if (age < 0) {
+    result.textContent = "Is this you're future life?";
+  } else if (age <= 18 && age >= 21) {
+    result.textContent = "Welcome to Young-Adulthood!";
+  }
+  else {
+    result.textContent = "You're a senior!";
+  }
+});
